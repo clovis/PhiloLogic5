79b741b03cd1ff545ff40942a875399f1f9d473d
Clovis Gladstone <clovisgladstone@gmail.com>
2018-06-06 21:59:14 -0500

    remove old python in libphilo

--- libphilo/python/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name = "philologic",
-      version = "4.0prototype",
-      author = "Richard Whaling",
-      author_email = "rwhaling@uchiacgo.edu",
-      packages = ["philologic", "philologic.utils"],
-     )
